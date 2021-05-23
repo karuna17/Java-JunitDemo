bbbbebbde02c039a9166c29c69a52c0a9a5c2e0b
karuna17 <karunakakade8@gmail.com>
2021-05-24 00:14:30 +0530

    Added: if passing parameter null then handle that exception with return HAPPY

--- MoodAnalyserDemo.java
+++ MoodAnalyserDemo.java
@@ -4,14 +4,14 @@ import org.junit.jupiter.api.Test;
 class MoodAnalyserDemo {	
 	@Test
 	public void testMoodAnalysis() {
-		
+
 		TestMood tm = new TestMood();
 		String mood="";
 		try {
-			 mood=tm.testMood(null);
+			mood=tm.testMood(null);
 			Assert.assertTrue(mood, true);
 		}catch (Exception e) {
-			e.printStackTrace();
+			System.out.println("HAPPY");
 		}
 		System.out.println(mood);
 		System.out.println("rest of the code executed");
